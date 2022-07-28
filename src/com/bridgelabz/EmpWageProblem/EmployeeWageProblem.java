dca8a597eea574a62b3ee7492210c5d262091cee
iamtalib13 <talibsheikh16@gmail.com>
2022-07-28 23:14:58 +0530

    Added - Part time Employee & Wage

--- src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
@@ -1,27 +1,39 @@
 package com.bridgelabz.EmpWageProblem;
 
 public class EmployeeWageProblem {
+    public static  final int ABSENT = 0;
     public static  final int FULL_TIME = 1;
+    public static final int PART_TIME = 2;
     public static final int WAGE_PER_HOUR = 20;
 
 
+
     public static void main(String[] args) {
-        int fullDayHrs = 0;
+        int empHrs = 0;
         int empWage = 0;
 
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        double empCheck = Math.floor(Math.random() * 10) %2;
+        int empCheck = (int) (Math.floor(Math.random() * 10) %3);
 
         if(empCheck == FULL_TIME)
-            System.out.println("Employee is Present");
-        else
+            System.out.println("Employee is Full Day Present");
+        else if(empCheck == PART_TIME)
+            System.out.println("Employee is Half Day Present");
+        else if(empCheck == ABSENT)
             System.out.println("Employee is Absent");
 
-        if(empCheck == FULL_TIME)
-            fullDayHrs = 8;
 
-        empWage = fullDayHrs * WAGE_PER_HOUR ;
+        if(empCheck == FULL_TIME)
+            empHrs = 8;
+        else if(empCheck == PART_TIME)
+            empHrs = 4;
+        else if (empCheck == ABSENT)
+            empHrs = 0;
+
+
+
+        empWage = empHrs * WAGE_PER_HOUR ;
         System.out.println("Employee wage is: "+ empWage);
     }
 }
